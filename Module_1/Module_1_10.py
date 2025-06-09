0bd05e09f529f48b2bfe67ea2d4ea13e196e63e4
Олег <solegva@ya.ru>
2025-06-09 10:48:36 +0300

    Module_1_10.py-v.0

---
+++ Module_1/Module_1_10.py
@@ -0,0 +1,40 @@
+# Задание!
+# Необходимо написать программу, которая будет считывать со стандартного ввода строку. Нужно
+# разбить строку на слова, словом будем считать последовательность символов отличных от
+# пробелов (то есть знаки пунктуации будут входить в слова). Далее нужно посчитать сколько
+# каждое слово встречалось в тексте и вывести наиболее часто слово и сколько оно встретилось.
+# Все слова нужно также приводить к нижнему регистру при подсчете. Гарантируется, что в
+# тексте самое частое слово – единственное.
+
+import  string
+line = input("")
+words = []
+current_word = ""
+for char in line:
+    if char.isalnum():
+        current_word += char
+    elif current_word:
+        words.append(current_word)
+        current_word = ""
+if current_word:
+    words.append(current_word)
+word_counts = {}
+for word in words:
+    word = word.lower()
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+most_common_word = ""
+most_common_count = 0
+for word, count in word_counts.items():
+    if count > most_common_count:
+        most_common_word = word
+        most_common_count = count
+
+# print(f"****Start of input***")
+# print(f"******End of input***")
+# print(f"*****-----------*****")
+# print(f"***Start of Result***")
+print(f"{most_common_word}, {most_common_count}")
+# print(f"*****End of Result***")
